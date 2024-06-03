06fa84bb81bb7260844ec0f3d8663f3aeb7c2653
Nicholas Kokotakis <njk51@georgetown.edu>
2024-06-03 12:38:26 -0400

    test

---
+++ test.js
@@ -0,0 +1,9 @@
+const PI = 3.14
+
+// reassign and redeclare with var, not redeclare with let
+
+// reassign and redeclare with var, not redeclare or reassign with const
+
+// reassign with let, not redeclare or reassign with const
+
+// Variables are lifting to the top of the scope they are declared in
